6b070e3203cd9227f4072f7525d4f4c40684bb99
Abdelali Mifdal <abdelali.mifdal@hotmail.com>
2017-05-27 20:01:13 +0100

    Ajout de la classe Tache (Model)

---
+++ Budgetis_V0/Models/Tache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Budgetis_V0.Models
+{
+    public class Tache
+    {
+        public int id { get; set; }
+        public String description { get; set; }
+        public double cout { get; set; }
+
+    }
+}
